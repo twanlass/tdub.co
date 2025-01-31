dd3acda49c157feb6729aae31798450d87b7b993
Tyler Wanlass <hello@tdub.co>
2025-01-30 20:17:56 -0800

    Maybe adds social share img? lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+    site: 'https://tyler.cv',
+    integrations: [tailwind(),  sitemap()]
 });
